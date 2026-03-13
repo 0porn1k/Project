3779bbbf081f0da08cc4e9a54f952b3461df401c
Дмитрий Портнов <portnovdr2007@gmail.com>
2026-03-13 17:41:55 +0300

    'xuy_polney'

---
+++ database.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 1. Указываем путь к файлу базы данных. 
+# SQLite создаст файл 'sql_app.db' в той же папке, где лежит этот скрипт.
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+
+# 2. Создаем "движок" (Engine)
+# connect_args={"check_same_thread": False} нужен только для SQLite
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+# 3. Создаем фабрику сессий. Через эти сессии мы будем делать запросы.
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# 4. Базовый класс для всех моделей (таблиц)
+Base = declarative_base()
+
+# Функция для получения сессии БД (её мы будем использовать в FastAPI через Depends)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
